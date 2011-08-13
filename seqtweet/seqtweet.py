b9ba94eeca96706d940c8e1d598a50c92db7ffe5
John J. Workman <johnjworkman@gmail.com>
2011-08-13 18:32:55 -0400

    Making lists with sequential replies

--- seqtweet/seqtweet.py
+++ seqtweet/seqtweet.py
@@ -4,6 +4,21 @@ import tweepy
 
 from creds import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
 
+def tweets_to_list(api, next_tweet):
+    l = []
+    done = False
+    while not done:
+        tweet = api.get_status(id=next_tweet)
+        if tweet.in_reply_to_status_id_str:
+            # Remove reply, which is (@ + handle + space).
+            elem = tweet.text[len(api.me().screen_name) + 2:]
+            next_tweet = tweet.in_reply_to_status_id_str
+        else:
+            elem = tweet.text
+            done = True
+        l.append(elem)
+    return l
+
 def list_to_tweets(api, l, max_payload=140):
     l.reverse()
     last_tweet = None
@@ -26,7 +41,9 @@ def main():
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
     api = tweepy.API(auth)
-    print list_to_tweets(api, l=range(4))
+    tweet_id = list_to_tweets(api, l="This is a seqtweet test.".split())
+    print "id=%s" % (tweet_id)
+    print "list=%s" % (tweets_to_list(api, tweet_id))
 
 if __name__ == '__main__':
     main()
