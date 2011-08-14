d7789392a32a65a4e492623beb4fcb0d13116ae5
John J. Workman <johnjworkman@gmail.com>
2011-08-14 03:08:41 -0400

    Building text-to-list function

--- seqtweet/seqtweet.py
+++ seqtweet/seqtweet.py
@@ -4,15 +4,14 @@ import tweepy
 
 from creds import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
 
-def tweets_to_list(api, next_tweet):
+def tweets_to_list(api, tweet_pointer):
     l = []
     done = False
     while not done:
-        tweet = api.get_status(id=next_tweet)
+        tweet = api.get_status(id=tweet_pointer)
         if tweet.in_reply_to_status_id_str:
-            # Remove reply, which is (@ + handle + space).
             elem = tweet.text[len(api.me().screen_name) + 2:]
-            next_tweet = tweet.in_reply_to_status_id_str
+            tweet_pointer = tweet.in_reply_to_status_id_str
         else:
             elem = tweet.text
             done = True
@@ -37,13 +36,39 @@ def list_to_tweets(api, l, max_payload=140):
         last_tweet = tweet.id_str
     return last_tweet
 
+def text_to_list(api, msg, max_payload=140):
+    l = []
+    chunk = ''
+    username = api.me().screen_name
+    split_msg = msg.split()
+    for word in split_msg:
+        if 1 + len(username) + 1 + len(word) > max_payload:
+            raise Exception("Word is too big.")
+        elif chunk == '':
+            chunk = word
+        elif 1 + len(username) + 1 + len(chunk) + 1 + len(word) > max_payload:
+            l.append(chunk)
+            chunk = word
+        else:
+            chunk = chunk + ' ' + word
+    l.append(chunk)
+    return l
+
 def main():
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
     api = tweepy.API(auth)
-    tweet_id = list_to_tweets(api, l="This is a seqtweet test.".split())
-    print "id=%s" % (tweet_id)
-    print "list=%s" % (tweets_to_list(api, tweet_id))
+    s = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do \
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim \
+        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut \
+        aliquip ex ea commodo consequat. Duis aute irure dolor in \
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla \
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in \
+        culpa qui officia deserunt mollit anim id est laborum.'
+    tweet_id = list_to_tweets(api, l=text_to_list(api, s))
+    print tweet_id
+    print "=>"
+    print ' '.join(tweets_to_list(api, tweet_id))
 
 if __name__ == '__main__':
     main()
