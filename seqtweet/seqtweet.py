5539833c431fed97992731b1a2f7a783c79f0d23
John J. Workman <johnjworkman@gmail.com>
2011-08-17 12:53:25 -0400

    Fixing unit test comparison

--- seqtweet/seqtweet.py
+++ seqtweet/seqtweet.py
@@ -103,13 +103,11 @@ def main():
     obj = SeqTweet(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
     s = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     print s
-    tweet_id = obj.create(s, ' ')
-    print "=>"
-    print tweet_id
-    print "=>"
-    data = obj.read(tweet_id, ' ')
+    tweet_id = obj.create(s, '')
+    print "=> %s =>" % (tweet_id)
+    data = obj.read(tweet_id, '')
     print data
-    print "Same? %s" % (data is s)
+    print "Same? %s" % (data == s)
 
 if __name__ == '__main__':
     main()
