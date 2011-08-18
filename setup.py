9f70e82ed640f3d53f59ce448eef3eccd4182fb3
John J. Workman <johnjworkman@gmail.com>
2011-08-18 15:56:31 -0400

    Adding PyPI and setup files

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="SeqTweet",
+    version="0.1",
+    author="John J. Workman",
+    author_email="workman@alumni.duke.edu",
+    url="https://github.com/workmajj/seqtweet",
+    description="Stores arbitrary strings on Twitter by implementing linked \
+        lists.",
+    license = "BSD 3-Clause <http://www.opensource.org/licenses/BSD-3-Clause>",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Operating System :: MacOS :: MacOS X',
+        'Topic :: Internet',
+        'Topic :: Utilities'
+    ],
+    install_requires=['Tweepy>=1.7.1'],
+    packages=['seqtweet']
+)
